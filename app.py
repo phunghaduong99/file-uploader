02c952c014568fb58dafc572d03c3204c3e2adca
phunghaduong99 <33689476+phunghaduong99@users.noreply.github.com>
2024-03-17 01:02:03 +0900

    API for create, del folder/file on gg drive

---
+++ app.py
@@ -0,0 +1,119 @@
+
+from flask import Flask
+from utils import list_folder, download_file, createRemoteFolder, upload_file,  deleteRemoteFolder, delete_files
+from flask import Flask, jsonify, redirect, request, Response
+from http import HTTPStatus
+import os
+from werkzeug.utils import secure_filename, redirect
+
+app = Flask(__name__)
+@app.route("/")
+def main():
+    return "Welcome!"
+
+############# FOLDER #################
+@app.route('/cloud/folder', methods=['POST'])
+def create_remote_folder_api():
+    data = request.get_json()
+    folderName = data.get('folderName', '')
+    if folderName == '':
+        return   Response(
+       "Please insert folder name.",
+        status= HTTPStatus.BAD_REQUEST,
+    ) 
+    parentFolderId = data.get('parentFolderId', '')
+    if parentFolderId == '':
+        return   Response(
+       "Please insert parentFolder Id.",
+        status= HTTPStatus.BAD_REQUEST,
+    ) 
+    else:
+        root_folder_id = createRemoteFolder(folderName, parentFolderId )
+        return jsonify({
+            'root_folder_id': root_folder_id
+        }), HTTPStatus.OK
+
+@app.route('/cloud/folder', methods=['DELETE'])
+def delete_remote_folder_api():
+    data = request.get_json()
+    folderId = data.get('folderId', '')
+    if folderId == '':
+        return   Response(
+       "Please insert folder Id.",
+        status= HTTPStatus.BAD_REQUEST,
+    ) 
+    else:
+        status = deleteRemoteFolder(folderId )
+        return jsonify({
+            'status': status
+        }), HTTPStatus.OK
+
+@app.route('/cloud/folder', methods=['GET'])
+def list_in_folder_api():
+    mimeType = request.args.get('mimeType', '')
+    return list_folder(mimeType)
+
+
+############# FILE #################
+@app.route('/cloud/file', methods=['DELETE'])
+def delete_remote_file_api():
+    data = request.get_json()
+    fileID = data.get('fileID', '')
+    if fileID == '':
+        return   Response(
+       "Please insert file Id.",
+        status= HTTPStatus.BAD_REQUEST,
+    ) 
+    else:
+        status = delete_files(fileID )
+        return jsonify({
+            'status': status
+        }), HTTPStatus.OK
+
+
+@app.route('/cloud/file/uploader', methods=['GET', 'POST'])
+def handle_request():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        f = request.files['file']
+        parentFolderId = request.form.get('parentFolderId')
+        if f:
+            f.save(secure_filename(f.filename))
+            file_id = upload_file(f, parentFolderId)
+            if os.path.isfile(secure_filename(f.filename)):
+                os.remove(secure_filename(f.filename))
+            return jsonify({
+                'file_id': file_id
+            }), HTTPStatus.OK
+    else:
+        return   Response(
+        "Error",
+        status= HTTPStatus.BAD_REQUEST)
+
+
+@app.route('/cloud/file/download', methods=['POST'])
+def download_file_api():
+    data = request.get_json()
+    fileId = data.get('fileId', '')
+    if fileId == '':
+        return   Response(
+       "Please insert file ID.",
+        status= HTTPStatus.BAD_REQUEST,
+    ) 
+    destinationPath = data.get('dest', '')
+    if destinationPath == '':
+        return   Response(
+       "Please insert destination path.",
+        status= HTTPStatus.BAD_REQUEST,
+    ) 
+    else:
+         download_file(fileId,  destinationPath)
+         return Response(
+       "OK",
+        status= HTTPStatus.ACCEPTED)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
---
+++ utils.py
@@ -0,0 +1,96 @@
+import io
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.http import MediaFileUpload
+from werkzeug.utils import secure_filename
+
+
+
+scope = ['https://www.googleapis.com/auth/drive']
+service_account_json_key = 'secret.json'
+credentials = service_account.Credentials.from_service_account_file(
+                              filename=service_account_json_key,
+                              scopes=scope)
+drive_service = build('drive', 'v3', credentials=credentials)
+# Call the Drive v3 API
+
+
+# get current datetime
+
+
+parent_id= "1RL5nVtP96hQ-wQPbKAH3B5JuYpNxZ_sV"
+PARENT_FOLDER_ID= "1RL5nVtP96hQ-wQPbKAH3B5JuYpNxZ_sV"
+workpath = "/home/hgtp_admin/mongodb-backup/backup/"
+backupfilename = "mongodb_dump.gz"
+
+# zip folder to file
+
+def upload_file(file, parentFolderId):
+    fileName = secure_filename(file.filename) 
+    file_metadata = {'name': fileName , 'parents': [parentFolderId]}
+    media = MediaFileUpload( fileName )
+    fileId = drive_service.files().create(body=file_metadata, media_body=media).execute()
+    return fileId
+    
+def delete_files(file_or_folder_id):
+    """Delete a file or folder in Google Drive by ID."""
+    try:
+        drive_service.files().delete(fileId=file_or_folder_id).execute()
+        print(f"Successfully deleted file/folder with ID: {file_or_folder_id}")
+    except Exception as e:
+        print(f"Error deleting file/folder with ID: {file_or_folder_id}")
+        print(f"Error details: {str(e)}")
+    return "OK"
+
+def download_file(file_id, destination_path):
+    file = drive_service.files().get(fileId=file_id).execute()
+    file_name = file.get("name")
+    print(f'File name is: {file_name}')
+    """Download a file from Google Drive by its ID."""
+    requestt = drive_service.files().get_media(fileId=file_id)
+    print(requestt)
+    fh = io.FileIO(destination_path +"/"+ file_name, mode='wb')
+    downloader = MediaIoBaseDownload(fh, requestt)
+    done = False
+    while not done:
+        status, done = downloader.next_chunk()
+        print(f"Download {int(status.progress() * 100)}%.")
+
+def createRemoteFolder(folderName, parentID = None):
+    # Create a folder on Drive, returns the newely created folders ID
+    body = {
+          'name': folderName,
+          'mimeType': "application/vnd.google-apps.folder"
+        }
+    if parentID:
+        body['parents'] = [parentID]
+    root_folder = drive_service.files().create(body = body, fields='id').execute()
+    return root_folder['id']
+
+def deleteRemoteFolder(folderId):
+    # Create a folder on Drive, returns the newely created folders ID
+    drive_service.files().delete(fileId=folderId).execute()
+    return 'OK'
+
+
+def list_folder(mimeType):
+    """List folders and files in Google Drive."""
+    results = drive_service.files().list(
+        q=f"'{PARENT_FOLDER_ID}' in parents and trashed=false" if PARENT_FOLDER_ID else None,
+        pageSize=1000,
+        fields="nextPageToken, files(id, name, mimeType)"
+    ).execute()
+    items = results.get('files', [])
+
+    if not items:
+        print("No folders or files found in Google Drive.")
+        return []
+    else:
+        print("Folders and files in Google Drive:")
+        items = [x for x in items if x['mimeType'] == mimeType]
+        for item in items:
+            print(f"Name: {item['name']}, ID: {item['id']}, Type: {item['mimeType']}")
+            # if delete:
+                # delete_files(item['id'])
+        return items
